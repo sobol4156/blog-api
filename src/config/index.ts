c9df8fa36f0c614ca957c4b7282ecdda272e70b7
alex <sobol4156@gmail.com>
2025-11-07 16:38:29 +0300

    feat(): add CORS

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config()
 
 const config = {
-  PORT: process.env.PORT || 3000
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  WHITELIST_ORIGINS: [""]
 }
 
 export default config;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,27 @@
 import express from 'express'
-import config from './config'
+import config from '@/config'
+import cors from 'cors'
+import type { CorsOptions } from 'cors'
 
 const app = express()
+
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (config.NODE_ENV === 'development' || !origin || config.WHITELIST_ORIGINS.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error(`CORS error: ${origin} is not allowed by CORS`), false)
+    }
+  }
+}
+
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hi'
+  })
+})
 
 app.listen(config.PORT, () => {
   console.log(`service running: http://localhost:${config.PORT}`)
