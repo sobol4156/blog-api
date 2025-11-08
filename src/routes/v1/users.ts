42cdeb0be2da92dc46fb947f72b224335aeb05b0
alex <sobol4156@gmail.com>
2025-11-09 00:41:30 +0300

    feat(users): add validation for get_all_user params part-2

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -19,6 +19,10 @@ router.get('',
     .optional()
     .isInt({ min: 1, max: 50 })
     .withMessage('Limit must be between 1 to 50'),
+  query('offset')
+    .optional()
+    .isInt({ min: 0})
+    .withMessage('Offset must be a positive integer'),
   validationError,
   getAllUser);
 
