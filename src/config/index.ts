c500b563d746d32cfd6af5762ad93c73b6eba2e7
alex <sobol4156@gmail.com>
2025-11-08 12:02:56 +0300

    feat(auth): add jwtwebtoken

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,15 +1,18 @@
 import dotenv from 'dotenv';
+import type ms from 'ms'
 
 dotenv.config()
-
-
 
 const config = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV,
   WHITELIST_ORIGINS: [""],
   MONGO_URI: `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`,
-  LOG_LEVEL: process.env.LOG_LEVEL || 'info'
+  LOG_LEVEL: process.env.LOG_LEVEL || 'info',
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as ms.StringValue,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as ms.StringValue,
 }
 
 export default config;
--- src/controllers/v1/auth/register.ts
+++ src/controllers/v1/auth/register.ts
@@ -2,6 +2,7 @@ import { logger } from '@/lib/winston'
 import User from '@/models/user'
 import config from '@/config'
 import { genUsername } from '@/utils'
+import { generateAccessToken, generateRefreshToken } from '@/lib/jwt'
 
 import type { IUser } from '@/models/user'
 import type { Request, Response } from 'express'
@@ -10,8 +11,6 @@ type UserData = Pick<IUser, 'email' | 'password' | 'role'>
 
 const register = async (req: Request, res: Response): Promise<void> => {
   const { email, password, role } = req.body as UserData
-
-  console.log(email, password, role)
 
   try {
     const username = genUsername();
@@ -23,14 +22,30 @@ const register = async (req: Request, res: Response): Promise<void> => {
       role
     })
 
+    const accessToken = generateAccessToken(newUser._id)
+    const refreshToken = generateRefreshToken(newUser._id)
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: config.NODE_ENV === 'production',
+      sameSite: 'strict'
+    })
+
     res.status(201).json({
       user: {
         username: newUser.username,
         email: newUser.email,
         role: newUser.role
       },
-      message: 'New user created'
+      accessToken,
     })
+
+    logger.info(`User reggistered successfully`, {
+      username: newUser.username,
+      email: newUser.email,
+      role: newUser.role
+    })
+
   } catch (err) {
     res.status(500).json({
       code: 'ServerError',
