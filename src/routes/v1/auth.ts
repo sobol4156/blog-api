651e232d01c2ced96feac611b8c11c46bc0a6ca8
alex <sobol4156@gmail.com>
2025-11-08 16:05:32 +0300

    feat(auth): add login route

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { body } from "express-validator";
+import bcrypt from 'bcrypt'
+
 import User from "@/models/user";
 import register from "@/controllers/v1/auth/register";
+import login from "@/controllers/v1/auth/login";
 import validationError from "@/middlewares/validationError";
 
 const router = Router()
@@ -35,5 +38,48 @@ router.post(`/register`,
     .withMessage('Role must be either admin or user'),
   validationError,
   register)
+
+router.post('/login',
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isLength({ max: 50 })
+    .withMessage('Email must be less than 50 characters')
+    .isEmail()
+    .withMessage('Invalid email address')
+    .custom(async (value) => {
+      const userExists = await User.exists({ email: value })
+
+      if (!userExists) {
+        throw new Error('User already exists')
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long')
+    .custom(async (value, { req }) => {
+      const { email } = req.body as { email: string };
+
+      const user = await User.findOne({ email })
+        .select('password')
+        .lean()
+        .exec()
+
+      if (!user) {
+        throw new Error('User email or password is invalid')
+      }
+
+      const passwordMatch = await bcrypt.compare(value, user.password)
+
+      if (!passwordMatch) {
+        throw new Error('User email or password is invalid')
+      }
+    }),
+  validationError,
+  login
+)
 
 export default router
