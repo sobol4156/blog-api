bba098c5d8ce2872cb7b8a1eeeeb7fd8f16bf15e
alex <sobol4156@gmail.com>
2025-11-09 18:32:13 +0300

    fix(blog): correct create_blog logic; fix(env): delete env file

--- src/controllers/v1/blog/create_blog.ts
+++ src/controllers/v1/blog/create_blog.ts
@@ -1,10 +1,45 @@
-import { DOMPurify } from 'dompurify';
 import type { Request, Response } from 'express';
-import { JSDOM } from 'jsdom';
+import DOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom'
 
 import { logger } from '@/lib/winston';
-import User from '@/models/user';
+import Blog, { IBlog } from '@/models/blog';
 
-const createBlog = async (req: Request, res: Response): Promise<void> => {};
+type BlogData = Pick<IBlog, 'title' | 'content' | 'banner' | 'status'>
 
-export default createBlog;
+const window = new JSDOM('').window;
+const purify = DOMPurify(window);
+
+const createBlog = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { title, content, banner, status } = req.body as BlogData
+    const userId = req.userId;
+
+    const cleanContent = purify.sanitize(content)
+
+    const newBlog = await Blog.create({
+      title,
+      content: cleanContent,
+      banner,
+      status,
+      author: userId
+    })
+
+    logger.info('New blog created', newBlog)
+
+    res.status(201).json({
+      blog: newBlog
+    })
+
+  } catch (err) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: err,
+    });
+
+    logger.error('Error during blog creation', err);
+  }
+}
+
+export default createBlog
