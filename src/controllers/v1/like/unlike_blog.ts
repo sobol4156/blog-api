2a111af97bf575822332b66d06ae0cc1b9d853ea
alex <sobol4156@gmail.com>
2025-11-10 23:01:36 +0300

    feat(like): add unlike_blog route

--- src/controllers/v1/like/unlike_blog.ts
+++ src/controllers/v1/like/unlike_blog.ts
@@ -4,11 +4,24 @@ import { logger } from '@/lib/winston';
 import Blog from '@/models/blog';
 import Like from '@/models/like';
 
-const likeBlog = async (req: Request, res: Response): Promise<void> => {
+const unLikeBlog = async (req: Request, res: Response): Promise<void> => {
   const { blogId } = req.params;
   const userId = req.params.userId;
 
   try {
+    const existingLike = await Like.findOne({ blogId, userId }).lean().exec();
+
+    if (!existingLike) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'Like not found',
+      });
+
+      return;
+    }
+
+    await Like.deleteOne({ _id: existingLike._id });
+
     const blog = await Blog.findById(blogId).select('likesCount').exec();
 
     if (!blog) {
@@ -19,23 +32,10 @@ const likeBlog = async (req: Request, res: Response): Promise<void> => {
       return;
     }
 
-    const existingLike = await Like.findOne({ blogId, userId }).lean().exec();
-
-    if (existingLike) {
-      res.status(400).json({
-        code: 'BadRequest',
-        message: 'You already liked this blog',
-      });
-
-      return;
-    }
-
-    await Like.create({ blogId, userId });
-
-    blog.likesCount++;
+    blog.likesCount--;
     await blog.save();
 
-    logger.info('Blog liked successfully', {
+    logger.info('Blog unliked successfully', {
       userId,
       blogId: blog._id,
       likesCount: blog.likesCount,
@@ -51,8 +51,8 @@ const likeBlog = async (req: Request, res: Response): Promise<void> => {
       error: err,
     });
 
-    logger.error('Error while liking blog', err);
+    logger.error('Error while unliking blog', err);
   }
 };
 
-export default likeBlog;
+export default unLikeBlog;
--- src/routes/v1/like.ts
+++ src/routes/v1/like.ts
@@ -5,6 +5,7 @@ import likeBlog from '@/controllers/v1/like/like_blog';
 import authenticate from '@/middlewares/authenticate';
 import authorize from '@/middlewares/authorize';
 import validationError from '@/middlewares/validationError';
+import unLikeBlog from '@/controllers/v1/like/unlike_blog';
 
 const router = Router();
 
@@ -15,6 +16,15 @@ router.post(
   param('blogId').isMongoId().withMessage('blogId is required'),
   validationError,
   likeBlog,
+);
+
+router.delete(
+  '/blog/:blogId',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('blogId').isMongoId().withMessage('blogId is required'),
+  validationError,
+  unLikeBlog,
 );
 
 export default router;
